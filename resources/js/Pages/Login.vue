c7eb14d1c19f7cc8ac8f7674c64b6c9ca0920109
Utsav <utsavsomaiya4464@gmail.com>
2025-04-16 17:28:48 +0530

    Install tailwind form plugins and use it

--- resources/js/Pages/Login.vue
+++ resources/js/Pages/Login.vue
@@ -1,10 +1,66 @@
 <template>
     <GuestLayout>
         <Head title="Login"></Head>
-    </GuestLayout>
 
+        <form @submit.prevent="submit">
+            <div>
+                <InputLabel for="email" value="Email" />
+
+                <TextInput
+                    id="email"
+                    type="email"
+                    class="mt-1 block w-full"
+                    v-model="form.email"
+                    required
+                    autofocus
+                    autocomplete="username"
+                />
+
+                <InputError class="mt-2" :message="form.errors.email" />
+            </div>
+
+            <div class="mt-4">
+                <InputLabel for="password" value="Password" />
+
+                <TextInput
+                    id="password"
+                    type="password"
+                    class="mt-1 block w-full"
+                    v-model="form.password"
+                    required
+                    autocomplete="current-password"
+                />
+
+                <InputError class="mt-2" :message="form.errors.password" />
+            </div>
+
+            <div class="mt-4 block">
+                <label class="flex items-center">
+                    <Checkbox name="remember" v-model:checked="form.remember" />
+                    <span class="ms-2 text-sm text-gray-600"
+                        >Remember me</span
+                    >
+                </label>
+            </div>
+        </form>
+    </GuestLayout>
 </template>
 <script setup>
-import { Head } from '@inertiajs/vue3'
+import { Head, useForm } from '@inertiajs/vue3'
 import GuestLayout from '../Layouts/GuestLayout.vue'
+import InputLabel from '../components/InputLabel.vue'
+import InputError from '../components/InputError.vue'
+import TextInput from '../components/TextInput.vue'
+
+const form = useForm({
+    email: '',
+    password: '',
+    remember: false,
+})
+
+const submit = () => {
+    form.post(route('login'), {
+        onFinish: () => form.reset('password'),
+    })
+}
 </script>
